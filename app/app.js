cfd3ecd495b5e80a7aa937d235905a772e090c1b
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-08-09 18:52:42 +0700

    chore: remove unused route

--- app/app.js
+++ app/app.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const uploadImageRouter = require('./routes/imageRouter');
 const uploadModelRouter = require('./routes/modelRouter');
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/', uploadImageRouter);
 app.use('/', uploadModelRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
