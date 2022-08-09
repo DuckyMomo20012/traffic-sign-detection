963a769be236c31348e016a7d2f5bddd57ec7f7f
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-08-09 19:12:47 +0700

    refactor: modelRouter.js

--- app/routes/modelRouter.js
+++ app/routes/modelRouter.js
@@ -1,64 +1,55 @@
 const express = require('express');
 
 const router = express.Router();
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const { uploadModel } = require('../services/multer');
 
-router.get('/upload-model', (req, res) => {
-  const hasModel = fs.existsSync(path.join(__dirname, '../../best.pt'));
+router.get('/upload-model', async (req, res) => {
+  const hasModel = await fs.pathExists(path.join(__dirname, '../../best.pt'));
   return res.render('upload-model', {
     hasModel,
   });
 });
 
+const EXT_ACCEPT = ['.pt', '.pth'];
+
 router.post(
   '/upload-model',
   uploadModel.single('data-model'),
   async (req, res) => {
-    if (req.file) {
-      const extAccept = ['.pt', '.pth'];
-      const extname = path.extname(req.file.originalname).toLowerCase();
-
-      if (extAccept.includes(extname)) {
-        await fs.rename(
-          path.join(__dirname, '../public/model/', req.file.originalname),
-          path.join(__dirname, '../public/model/', 'best.pt'),
-          (err) => {
-            if (err) throw err;
-          },
-        );
-        if (await fs.existsSync(path.join(__dirname, '../../best.pt'))) {
-          await fs.unlinkSync(path.join(__dirname, '../../best.pt'));
-        }
-
-        const path_src = path.join(__dirname, '../public/model/best.pt');
-        const path_dst = path.join(__dirname, '../../best.pt');
-        await fs.copyFile(path_src, path_dst, (err) => {
-          if (err) {
-            console.log(err);
-          }
-          console.log('Copy file done.');
-        });
-        const justAdd = true;
-        return res.render('upload-model', {
-          justAdd,
-        });
-      }
-      if (await fs.existsSync(path.join(__dirname, '../../best.pt'))) {
-        await fs.unlinkSync(path.join(__dirname, '../../best.pt'));
-      }
-      await fs.unlinkSync(
-        path.join(__dirname, '../../', req.file.originalname),
-      );
-      const notSupport = true;
+    if (!req.file) {
       return res.render('upload-model', {
-        notSupport,
+        notFile: true,
       });
     }
-    const notFile = true;
+
+    const extname = path.extname(req.file.originalname).toLowerCase();
+
+    if (!EXT_ACCEPT.includes(extname)) {
+      return res.render('upload-model', {
+        notSupport: true,
+      });
+    }
+
+    await fs.move(
+      path.join(__dirname, '../public/model/', req.file.originalname),
+      path.join(__dirname, '../public/model/', 'best.pt'),
+      {
+        overwrite: true,
+      },
+    );
+
+    await fs.move(
+      path.join(__dirname, '../public/model/best.pt'),
+      path.join(__dirname, '../../yolo/best.pt'),
+      {
+        overwrite: true,
+      },
+    );
+
     return res.render('upload-model', {
-      notFile,
+      justAdd: true,
     });
   },
 );
