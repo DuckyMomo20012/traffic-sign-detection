62b8f85fc9b6f8b1b2c5e6ef6af0692a4ae14211
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-08-11 12:17:29 +0700

    chore: remove unused files

--- app/public/js/loading.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("run-model").onclick = function (event) {
-    event.preventDefault();
-    document.getElementById("run-model-loading").style.display = "block";
-    this.form.submit();
-}
