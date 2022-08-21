bf2988dc5502de706a623870aad9a9f98d617d2d
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-08-21 09:50:56 +0700

    chore: remove redundant files

--- web/src/pages/api/auth/index.js
+++
@@ -1,9 +0,0 @@
-import { sha1 } from 'hash-wasm';
-
-const getOneUser = async () => {
-  const password = await sha1('World');
-  const users = await Promise.resolve({ name: 'Hello', password });
-  return users;
-};
-
-export { getOneUser };
