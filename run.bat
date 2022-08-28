e8c8c825599a2e4e34be990dfeaa7d896e1402f0
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-08-28 21:01:16 +0700

    ci: update run scripts

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 @echo off
-start cmd.exe /k "cd app & npm start"
-start cmd.exe /k ".venv\Scripts\activate & python yolo.py"
+start cmd.exe /k "cd web & yarn dev"
+start cmd.exe /k "cd app & yarn dev"
+start cmd.exe /k ".venv\Scripts\activate & python script.py"
