258870fbe2fa2cac79589f6efee0dc4e8bd6221f
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-09-04 21:07:02 +0700

    refactor: setup nx
    refactor: add "web" app to nx workspace

--- app/constants/constants.js
+++
@@ -1,5 +0,0 @@
-const IMG_ACCEPT = ['.png', '.jpeg', '.jpg', '.webp'];
-
-const MIME_TYPE_ACCEPT = ['image/png', 'image/jpeg', 'image/webp'];
-
-module.exports = { IMG_ACCEPT, MIME_TYPE_ACCEPT };
--- yolo/script.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-venv_python = "./.venv/bin/python3"
-args = [venv_python, "run.py"]
-subprocess.run(args)
