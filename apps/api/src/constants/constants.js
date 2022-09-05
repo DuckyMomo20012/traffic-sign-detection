64a02bf098ce2cea697e5d078706cd8fd3869cf4
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-09-05 11:19:26 +0700

    chore(api): resolve fixed upload and result dir

--- apps/api/src/constants/constants.js
+++ apps/api/src/constants/constants.js
@@ -1,5 +1,11 @@
+const path = require('path');
+
 const IMG_ACCEPT = ['.png', '.jpeg', '.jpg', '.webp'];
 
 const MIME_TYPE_ACCEPT = ['image/png', 'image/jpeg', 'image/webp'];
 
-module.exports = { IMG_ACCEPT, MIME_TYPE_ACCEPT };
+const UPLOAD_DIR = path.join(__dirname, '../../../shared/assets/upload/');
+
+const RESULT_DIR = path.join(__dirname, '../../../shared/assets/result/');
+
+module.exports = { IMG_ACCEPT, MIME_TYPE_ACCEPT, UPLOAD_DIR, RESULT_DIR };
