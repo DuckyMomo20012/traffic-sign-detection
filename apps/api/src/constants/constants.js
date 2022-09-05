b91611df8984c510a6d3208db68fb6a190f2f000
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-09-05 22:06:32 +0700

    refactor: move shared lib to libs folder
    chore: config app's dependencies

--- apps/api/src/constants/constants.js
+++ apps/api/src/constants/constants.js
@@ -4,8 +4,14 @@ const IMG_ACCEPT = ['.png', '.jpeg', '.jpg', '.webp'];
 
 const MIME_TYPE_ACCEPT = ['image/png', 'image/jpeg', 'image/webp'];
 
-const UPLOAD_DIR = path.join(__dirname, '../../../shared/assets/upload/');
+const UPLOAD_DIR = path.join(
+  __dirname,
+  '../../../../libs/shared/assets/src/upload/',
+);
 
-const RESULT_DIR = path.join(__dirname, '../../../shared/assets/result/');
+const RESULT_DIR = path.join(
+  __dirname,
+  '../../../../libs/shared/assets/src/result/',
+);
 
 module.exports = { IMG_ACCEPT, MIME_TYPE_ACCEPT, UPLOAD_DIR, RESULT_DIR };
